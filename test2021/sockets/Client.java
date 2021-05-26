5c7be880850623ef424009e8c3399d13598e28c5
SillyFreak <sillyfreak.ck@gmail.com>
2021-05-26 12:18:25 +0200

    single-threaded Client & Server
    Server-Klasse: 6/8 Pkt
    Client-Klasse: 4/4 Pkt
    Logik: 0/8 Pkt
    EK: 0/4 Pkt

---
+++ test2021/sockets/Client.java
@@ -0,0 +1,21 @@
+package test2021.sockets;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class Client {
+    public static void main(String[] args) {
+        try (
+            Socket socket = new Socket("localhost", 5000);
+            BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            PrintWriter writer = new PrintWriter(socket.getOutputStream(), true);
+        ) {
+            // TODO
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+}
---
+++ test2021/sockets/Server.java
@@ -0,0 +1,35 @@
+package test2021.sockets;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+    public static void main(String[] args) {
+        try (
+            ServerSocket server = new ServerSocket(5000);
+        ) {
+            while (true) {
+                Socket socket = server.accept();
+                handleClient(socket);
+            }
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    private static void handleClient(Socket socket) {
+        try (
+            // socket;
+            BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            PrintWriter writer = new PrintWriter(socket.getOutputStream(), true);
+        ) {
+            System.out.println("todo");
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+}
